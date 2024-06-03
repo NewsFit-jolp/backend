eadcc8c18ed0a7689ecb0069b1a139dfe3bdf7dc
muhsroom1324 <mushroom1324@naver.com>
2024-06-03 14:29:33 +0900

    feature: initial settings

---
+++ src/test/java/com/example/daycarat/NewsFitApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.daycarat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsFitApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+
+}
