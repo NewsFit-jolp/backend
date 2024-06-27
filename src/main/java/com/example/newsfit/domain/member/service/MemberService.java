0e5261425e48e0f1a929efb45681e20169447366
myName <kimsungjin927@g.hongik.ac.kr>
2024-06-27 16:30:54 +0900

    refactor: registerKakaoUserIfNeed -> registerMemberIfNeed로 리팩토링

--- src/main/java/com/example/newsfit/domain/member/service/MemberService.java
+++ src/main/java/com/example/newsfit/domain/member/service/MemberService.java
@@ -1,11 +1,14 @@
 package com.example.newsfit.domain.member.service;
 
 import com.example.newsfit.domain.member.dto.GetMemberInfo;
+import com.example.newsfit.domain.member.dto.MemberDto;
 import com.example.newsfit.domain.member.entity.Member;
+import com.example.newsfit.domain.member.entity.Role;
 import com.example.newsfit.domain.member.repository.MemberRepository;
 import com.example.newsfit.global.error.exception.CustomException;
 import com.example.newsfit.global.error.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.tuple.Pair;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
@@ -33,5 +36,31 @@ public class MemberService {
         memberRepository.delete(member);
 
         return true;
+    }
+
+    public Pair<Member, Boolean> registerMemberIfNeed(MemberDto MemberInfo) {
+
+        String memberEmail = MemberInfo.getEmail();
+        String memberNickname = MemberInfo.getNickname();
+        String memberProfileImage = MemberInfo.getProfileImage();
+
+        Member member = memberRepository.findByEmail(memberEmail)
+                .orElse(null);
+
+        if (member == null) {
+
+            member = Member.builder()
+                    .email(memberEmail)
+                    .nickname(memberNickname)
+                    .profileImage(memberProfileImage)
+                    .role(Role.USER)
+                    .build();
+
+            memberRepository.save(member);
+
+            return Pair.of(member, true);
+
+        }
+        return Pair.of(member, false);
     }
 }
--- src/main/java/com/example/newsfit/global/jwt/TokenService.java
+++ src/main/java/com/example/newsfit/global/jwt/TokenService.java
@@ -72,7 +72,7 @@ public class TokenService {
         호출: RequestHeader("Authorization")의 String token을 그대로(Bearer 안떼고) 넣는다
         반환: 해당 Member 객체를 반환한다.
      */
-    public Member getUser(String token) {
+    public Member getMember(String token) {
         // 토큰으로부터 이메일을 얻음
         token = token.split(" ")[1].trim();
         String email = getEmail(token);
