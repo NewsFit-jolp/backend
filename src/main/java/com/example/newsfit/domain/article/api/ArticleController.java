b3d3f9c3b61af0e1baab88b0c933e34f2468a392
myName <kimsungjin927@g.hongik.ac.kr>
2024-09-12 16:34:57 +0900

    feat: 댓글 작성 API 작성

--- src/main/java/com/example/newsfit/domain/article/api/ArticleController.java
+++ src/main/java/com/example/newsfit/domain/article/api/ArticleController.java
@@ -1,11 +1,13 @@
 package com.example.newsfit.domain.article.api;
 
 import com.example.newsfit.domain.article.dto.GetArticle;
+import com.example.newsfit.domain.article.dto.GetComment;
 import com.example.newsfit.domain.article.entity.Article;
 import com.example.newsfit.domain.article.service.ArticleService;
 import com.example.newsfit.global.response.SuccessResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import net.minidev.json.parser.ParseException;
 import org.springframework.web.bind.annotation.*;
@@ -31,7 +33,7 @@ public class ArticleController {
     @Operation(summary = "뉴스 조회",
             description = """
                     뉴스 조회 API입니다.
-                                        
+                    
                     파라미터는 다음과 같습니다.
                     category: 조회하고자 하는 기사의 카테고리를 지정합니다.
                     press: 조회하고자 하는 기사의 언론사를 지정합니다.
@@ -53,5 +55,16 @@ public class ArticleController {
     @DeleteMapping("/{articleId}")
     public SuccessResponse<Boolean> deleteArticle(@PathVariable("articleId") String articleId) {
         return SuccessResponse.success(articleService.removeArticle(Long.parseLong(articleId)));
+    }
+
+    @Operation(summary = "댓글 작성",
+            description = """
+                    댓글 작성 API입니다.
+                    """)
+    @PostMapping("/{articleId}/comments/{parentCommentId}")
+    public SuccessResponse<GetComment> postComment(@PathVariable("articleId") String articleId,
+                                                   @PathVariable(value = "parentCommentId", required = false) String parentCommentId,
+                                                   @RequestBody String requestBody) throws ParseException {
+        return SuccessResponse.createSuccess(articleService.postComment(articleId, parentCommentId, requestBody));
     }
 }
