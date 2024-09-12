360a1ee6f5e19f591b8f875838820252c6fe0cc9
myName <kimsungjin927@g.hongik.ac.kr>
2024-09-12 16:58:39 +0900

    chore: PR 템플릿 작성

--- src/main/java/com/example/newsfit/global/jwt/TokenService.java
+++ src/main/java/com/example/newsfit/global/jwt/TokenService.java
@@ -12,7 +12,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
