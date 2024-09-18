1ad1f9d58e2d9f530f5b5a0610de27d09c5fda2f
myName <kimsungjin927@g.hongik.ac.kr>
2024-09-19 04:03:58 +0900

    feat: 게시글 리스트 조회와 단건 조회 분리

--- src/main/java/com/example/newsfit/domain/article/dto/GetArticle.java
+++ src/main/java/com/example/newsfit/domain/article/dto/GetArticle.java
@@ -2,24 +2,37 @@ package com.example.newsfit.domain.article.dto;
 
 import com.example.newsfit.domain.article.entity.Article;
 import com.example.newsfit.domain.article.entity.Category;
+import com.example.newsfit.domain.article.entity.Comment;
 import com.example.newsfit.domain.article.entity.Press;
 import io.swagger.v3.oas.annotations.media.Schema;
 
+import java.util.ArrayList;
+import java.util.List;
 
 public record GetArticle(
-        @Schema(description = "기사 제목", example = "기사 제목") String title,
-        @Schema(description = "기사 내용", example = "기사 내용") String content ,
-        @Schema(description = "언론사", example = "Chosun") Press press,
-        @Schema(description = "카테고리", example = "IT") Category category
-
+        @Schema(description = "제목") String title,
+        @Schema(description = "내용") String content,
+        @Schema(description = "언론사") Press press,
+        @Schema(description = "카테고리") Category category,
+        @Schema(description = "댓글") List<GetComment> comment
 ) {
-    public static GetArticle of (Article article){
+    private static List<GetComment> getComments(Article article) {
+        List<GetComment> commentDtoList = new ArrayList<>();
+        List<Comment> comments = article.getComments();
+
+        for (Comment comment : comments) {
+            commentDtoList.add(GetComment.of(comment));
+        }
+
+        return commentDtoList;
+    }
+    public static GetArticle of(Article article) {
         return new GetArticle(
                 article.getTitle(),
                 article.getContent(),
                 article.getPress(),
-                article.getCategory()
+                article.getCategory(),
+                getComments(article)
         );
     }
 }
-
