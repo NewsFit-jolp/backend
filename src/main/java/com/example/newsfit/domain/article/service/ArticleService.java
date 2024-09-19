71f829eb4660b71f4b01a3eb88cc2f4eb004a977
myName <kimsungjin927@g.hongik.ac.kr>
2024-09-19 21:46:04 +0900

    feat: 게시글 좋아요 API 구현

--- src/main/java/com/example/newsfit/domain/article/service/ArticleService.java
+++ src/main/java/com/example/newsfit/domain/article/service/ArticleService.java
@@ -3,10 +3,8 @@ package com.example.newsfit.domain.article.service;
 import com.example.newsfit.domain.article.dto.GetArticle;
 import com.example.newsfit.domain.article.dto.GetArticles;
 import com.example.newsfit.domain.article.dto.GetComment;
-import com.example.newsfit.domain.article.entity.Article;
-import com.example.newsfit.domain.article.entity.Category;
-import com.example.newsfit.domain.article.entity.Comment;
-import com.example.newsfit.domain.article.entity.Press;
+import com.example.newsfit.domain.article.entity.*;
+import com.example.newsfit.domain.article.repository.ArticleLikesRepository;
 import com.example.newsfit.domain.article.repository.ArticleRepository;
 import com.example.newsfit.domain.article.repository.CommentRepository;
 import com.example.newsfit.domain.member.entity.Member;
@@ -24,6 +22,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.webjars.NotFoundException;
 
 import java.util.List;
+import java.util.Optional;
 
 import static com.example.newsfit.global.util.Utils.jsonObjectParser;
 
@@ -35,6 +34,7 @@ public class ArticleService {
     private final ArticleRepository articleRepository;
     private final MemberRepository memberRepository;
     private final CommentRepository commentRepository;
+    private final ArticleLikesRepository articleLikesRepository;
 
     public GetArticles postArticle(String requestBody) throws ParseException {
         JSONObject jsonObject = jsonObjectParser(requestBody);
@@ -136,6 +136,29 @@ public class ArticleService {
         }
 
         return comment.deleteComment();
+    }
+
+    public Boolean postArticleLikes(String articleId) {
+        Article article = articleRepository.findById(Long.parseLong(articleId))
+                .orElseThrow(() -> new CustomException(ErrorCode.ARTICLE_NOT_FOUND));
+
+        Member member = memberRepository.findByMemberId(SecurityContextHolder.getContext().getAuthentication().getName())
+                .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
+
+
+        if(articleLikesRepository.findByMemberAndArticle(member, article).isPresent()){
+            throw new CustomException(ErrorCode.DUPLICATED_ARTICLE_LIKE);
+        }
+
+        ArticleLike articleLike = ArticleLike.builder()
+                .article(article)
+                .member(member)
+                .build();
+
+        articleLikesRepository.save(articleLike);
+        article.addLikeCount();
+
+        return true;
     }
 }
 
