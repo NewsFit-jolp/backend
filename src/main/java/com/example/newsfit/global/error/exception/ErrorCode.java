dc0b9f46cfa88afd1612e80d0c3786cf4cc68c36
myName <kimsungjin927@g.hongik.ac.kr>
2024-09-19 21:45:01 +0900

    feat: 좋아요 중복 시 발생하는 예외 작성

--- src/main/java/com/example/newsfit/global/error/exception/ErrorCode.java
+++ src/main/java/com/example/newsfit/global/error/exception/ErrorCode.java
@@ -17,7 +17,8 @@ public enum ErrorCode {
     WITHDRAWAL_USER(401, "C006", "탈퇴한 사용자입니다."),
     ARTICLE_NOT_FOUND(404, "C007", "존재하지 않는 게시글입니다."),
     COMMENT_NOT_FOUND(404, "C008", "존재하지 않는 댓글입니다."),
-    COMMENT_DELETE_FORBIDDEN(403, "C009", "댓글 삭제 권한이 없는 사용자입니다.");
+    COMMENT_DELETE_FORBIDDEN(403, "C009", "댓글 삭제 권한이 없는 사용자입니다."),
+    DUPLICATED_ARTICLE_LIKE(400, "C010", "이미 좋아요를 누른 게시글입니다.");
 
 
     private final int status;
