da30e7b0cc0536d05dfc29edd87cddda409c2ede
myName <kimsungjin927@g.hongik.ac.kr>
2024-09-24 17:24:21 +0900

    feat: 댓글 조회 시 좋아요 수 보이도록 구현

--- src/main/java/com/example/newsfit/domain/article/dto/GetComment.java
+++ src/main/java/com/example/newsfit/domain/article/dto/GetComment.java
@@ -9,6 +9,7 @@ public record GetComment(
         @Schema(description = "댓글 아이디", example = "1") Long commentId,
         @Schema(description = "댓글 내용", example = "댓글입니다.") String content,
         @Schema(description = "작성자") String nickName,
+        @Schema(description = "좋아요 수") Integer likeCount,
         @Schema(description = "등록 일자", example = "2024-08-31T12:00:00 000") LocalDateTime createdDate,
         @Schema(description = "삭제 여부", example = "false") Boolean isDeleted) {
 
@@ -17,6 +18,7 @@ public record GetComment(
                 comment.getComment_id(),
                 comment.getContent(),
                 comment.getMember().getNickname(),
+                comment.getLikeCount(),
                 comment.getCreatedDate(),
                 comment.getIsDeleted()
         );
