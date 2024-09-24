7624ba0ead095d5f9e1f78812aaee8156737da95
myName <kimsungjin927@g.hongik.ac.kr>
2024-09-24 17:22:50 +0900

    feat: 댓글 좋아요 엔티티 작성

---
+++ src/main/java/com/example/newsfit/domain/article/entity/CommentLike.java
@@ -0,0 +1,25 @@
+package com.example.newsfit.domain.article.entity;
+
+import com.example.newsfit.domain.member.entity.Member;
+import jakarta.persistence.*;
+import lombok.Builder;
+
+public class CommentLike {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "comment_id")
+    private Comment comment;
+
+    @Builder
+    public CommentLike(Member member, Comment comment) {
+        this.member = member;
+        this.comment = comment;
+    }
+}
