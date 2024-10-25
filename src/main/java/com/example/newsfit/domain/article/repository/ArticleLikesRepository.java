d30cc5dc7c937d3635d55a5b366f08119adf18a1
myName <kimsungjin927@g.hongik.ac.kr>
2024-10-26 04:55:44 +0900

    feat: 좋아요 여부 확인하는 메서드 작성

--- src/main/java/com/example/newsfit/domain/article/repository/ArticleLikesRepository.java
+++ src/main/java/com/example/newsfit/domain/article/repository/ArticleLikesRepository.java
@@ -10,5 +10,7 @@ import java.util.Optional;
 public interface ArticleLikesRepository extends JpaRepository<ArticleLike, Long> {
     Optional<ArticleLike> findByMemberAndArticle(Member member, Article article);
 
+    Boolean existsByMember_MemberIdAndArticle(String memberId, Article article);
+
     Optional<Integer> removeByMemberAndArticle(Member member, Article article);
 }
