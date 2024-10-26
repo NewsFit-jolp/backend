b7107ab166e20fd46cc450b0446ca63c93169bca
myName <kimsungjin927@g.hongik.ac.kr>
2024-10-27 03:39:22 +0900

    fix: 카테고리 한글로 수정

--- src/main/java/com/example/newsfit/domain/article/entity/Category.java
+++ src/main/java/com/example/newsfit/domain/article/entity/Category.java
@@ -1,5 +1,5 @@
 package com.example.newsfit.domain.article.entity;
 
 public enum Category {
-    POLITICS, ECONOMY, SOCIAL, LIFE, CULTURE, WORLD, TECHNOLOGY, IT, ENTERTAINMENT, SPORTS
+    정치, 경제, 사회, 생활_문화, 세계, 기술_IT, 연예, 스포츠
 }
