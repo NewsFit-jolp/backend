0d0b240f6c4d4f330368727a8c2fbaa2e6859669
myName <kimsungjin927@g.hongik.ac.kr>
2024-10-28 15:58:40 +0900

    fix: 카테고리 enum 적용

--- src/main/java/com/example/newsfit/domain/article/dto/GetArticles.java
+++ src/main/java/com/example/newsfit/domain/article/dto/GetArticles.java
@@ -13,7 +13,7 @@ public record GetArticles(
         @Schema(description = "기사 제목", example = "기사 제목") String title,
         @Schema(description = "헤드라인", example = "헤드라인") String headLine,
         @Schema(description = "언론사", example = "Chosun") Press press,
-        @Schema(description = "카테고리", example = "IT") Category category,
+        @Schema(description = "카테고리", example = "IT") String category,
         @Schema(description = "썸네일", example = "www.example.com/images/1") String thumbnail,
         @Schema(description = "원본 기사의 게시일", example = "2024-10-25T10:00") LocalDateTime publishDate
 ) {
@@ -25,7 +25,7 @@ public record GetArticles(
                 article.getTitle(),
                 article.getHeadLine(),
                 article.getPress(),
-                article.getCategory(),
+                article.getCategory().toString(),
                 image,
                 article.getPublishDate()
         );
