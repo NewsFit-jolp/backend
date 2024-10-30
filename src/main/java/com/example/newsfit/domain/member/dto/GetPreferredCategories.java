4efa2ae491c84ce9a9320eed924e711e8d05b095
myName <kimsungjin927@g.hongik.ac.kr>
2024-10-30 14:21:52 +0900

    fix: dto 반환명 변경

--- src/main/java/com/example/newsfit/domain/member/dto/GetPreferredCategories.java
+++ src/main/java/com/example/newsfit/domain/member/dto/GetPreferredCategories.java
@@ -1,16 +1,22 @@
 package com.example.newsfit.domain.member.dto;
 
+import com.amazonaws.services.dynamodbv2.xspec.S;
 import com.example.newsfit.domain.article.entity.Category;
 import com.example.newsfit.domain.member.entity.*;
 import io.swagger.v3.oas.annotations.media.Schema;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public record GetPreferredCategories(
-        @Schema(description = "선호 카테고리", example = "IT, Technology, Sports") List<Category> preferredCategories
-
+        @Schema(description = "선호 카테고리", example = "IT, Technology, Sports") List<String> preferredCategories
         ) {
     public static GetPreferredCategories of(Member member) {
-        return new GetPreferredCategories(member.getPreferredCategories());
+        List<Category> preferredCategories = member.getPreferredCategories();
+        List<String> displayName = new ArrayList<>();
+        for (Category category : preferredCategories) {
+            displayName.add(category.toString());
+        }
+        return new GetPreferredCategories(displayName);
     }
 }
