acabe5f8bb7ea4fddebd623ec0ec890361cded58
myName <kimsungjin927@g.hongik.ac.kr>
2024-10-30 13:25:21 +0900

    refactor: 추천 서버에 httpRequest 보내는 부분 메서드로 분리

--- src/main/java/com/example/newsfit/global/util/RecommenderUtils.java
+++ src/main/java/com/example/newsfit/global/util/RecommenderUtils.java
@@ -1,14 +1,16 @@
 package com.example.newsfit.global.util;
 
+import com.example.newsfit.domain.article.entity.Press;
+import com.example.newsfit.domain.member.entity.Member;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import net.minidev.json.JSONArray;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.Map;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -19,59 +21,20 @@ public class RecommenderUtils {
     @Value("${recommender.endpoint}")
     private String recommenderEndpoint;
 
-    public Boolean registerMember(Long memberId) throws JsonProcessingException {
-        HttpHeaders headers = new HttpHeaders();
-        headers.set(HttpHeaders.CONTENT_TYPE, "application/json");
-
-        ObjectMapper objectMapper = new ObjectMapper();
+    public void registerMember(Long memberId) throws JsonProcessingException {
         String requestBody = String.format("{ \"user_id\": %d }", memberId);
-
-        HttpEntity<String> request = new HttpEntity<>(requestBody, headers);
-
-        ResponseEntity<String> response = restTemplate.exchange(
-                recommenderEndpoint + "/new-user",
-                HttpMethod.POST,
-                request,
-                String.class
-        );
-
-        if (response.getStatusCode() == HttpStatus.OK) {
-            String responseBody = response.getBody();
-            Map<String, Object> responseMap = objectMapper.readValue(responseBody, Map.class);
-            return responseMap.get("status").equals("new user added");
-        } else {
-            throw new RuntimeException("Failed to send POST request: " + response.getStatusCode());
-        }
+        requestRecommender(requestBody, "new-user", HttpMethod.POST);
     }
 
     public void deleteMember(Long memberId) throws JsonProcessingException {
-        HttpHeaders headers = new HttpHeaders();
-        headers.set(HttpHeaders.CONTENT_TYPE, "application/json");
-
         String requestBody = String.format("{ \"user_id\": %d }", memberId);
-
-        HttpEntity<String> request = new HttpEntity<>(requestBody, headers);
-
-        ResponseEntity<String> response = restTemplate.exchange(
-                recommenderEndpoint + "/delete-user",
-                HttpMethod.DELETE,
-                request,
-                String.class
-        );
-
-        if (response.getStatusCode() != HttpStatus.OK) {
-            throw new RuntimeException("Failed to send DELETE request: " + response.getStatusCode());
-        }
+        requestRecommender(requestBody, "delete-user", HttpMethod.DELETE);
     }
 
-    public Boolean removeOldArticles(Long newsId) throws JsonProcessingException {
-        HttpHeaders headers = new HttpHeaders();
-        headers.set(HttpHeaders.CONTENT_TYPE, "application/json");
-
-        ObjectMapper objectMapper = new ObjectMapper();
+    public void removeOldArticles(Long newsId) throws JsonProcessingException {
         String requestBody = String.format("{ \"news_id\": %d }", newsId);
-
-        HttpEntity<String> request = new HttpEntity<>(requestBody, headers);
+        requestRecommender(requestBody, "delete-news", HttpMethod.DELETE);
+    }
 
         ResponseEntity<String> response = restTemplate.exchange(
                 recommenderEndpoint + "/delete-news",
@@ -87,6 +50,23 @@ public class RecommenderUtils {
         } else {
             throw new RuntimeException("Failed to send DELETE request: " + response.getStatusCode());
         }
+    }
 
+    private void requestRecommender(String requestBody, String path, HttpMethod method) throws JsonProcessingException {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set(HttpHeaders.CONTENT_TYPE, "application/json");
+
+        HttpEntity<String> request = new HttpEntity<>(requestBody, headers);
+
+        ResponseEntity<String> response = restTemplate.exchange(
+                recommenderEndpoint + path,
+                method,
+                request,
+                String.class
+        );
+
+        if (HttpStatus.OK != response.getStatusCode()) {
+            throw new RuntimeException("Failed to send request: " + response.getStatusCode());
+        }
     }
 }
